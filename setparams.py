a573acf195807951d981160092698f58afc8ed01
Lisa Lowe <lllowe@ncsu.edu>
2022-02-10 09:54:29 -0500

    Compare any two runs

--- setparams.py
+++ setparams.py
@@ -3,6 +3,13 @@
 import sys
 
 #-------- User Modified Section ------
+# Name of run
+# Choose a string - no spaces! - to identify the run.  
+# It will be added to the name of the images.
+# You can leave it empty by using two double quotes with no space.
+RUN_NAME = "sinking"
+
+
 # How many mi files are available?
 # Change this to look at the path and calculate
 NUM_MI_FILES = 13
@@ -11,48 +18,72 @@ NUM_MI_FILES = 13
 # If not, it will do every single timestep of every file
 do_first_in_file = 1
 
-#----  Which plot(s)?
+# Are you comparing two runs? 
+do_compare = 1
+
+# Is the comparison dataset from Mark
+# If so, TP will be defined in terms of NDZP
+do_MDR = 0
+
+
+#--- Which plot(s)?
 ## 3D pseudocolor plot
 do_3Dplot = 1
 ## 2D pseudocolor slice
 do_2Dslice = 1
 ## 2D transect
 do_2Dtransect = 1
-## Error check: Pick at least 1
-if (do_3Dplot+do_2Dslice+do_2Dtransect) < 1:
-    sys.exit("Pick at least 1 plot in setparams.py.\n")
 
-#----  Pick points for transect
+#--- Pick points for transect
 ## If you won't do a transect, just ignore - it won't try to use the values
 FROM_X = 561848.5
 FROM_Y = 4756940.5
 TO_X = 548466.63
 TO_Y = 4793653.0
 
-#--- set min/max for colormap
-## For both TP_EPA and TP_Mark
+#--- Plot parameters
+##---  If only doing 1 plot:
+# Set min/max for colormap
+# This will be used in comparison plots as well
 MIN_TP = 0.00
 MAX_TP = 0.02
+##--  Titles
+TITLE_TP_EPA = "TP_EPA"
+UNITS_TP_EPA = "(mg/L)"
+##--- If doing comparison plots 
+## If you won't do a comparison, just ignore - it won't try to use the values
+##-- Set min/max for colormaps
 ## Difference colormap
-MIN_TP_diff = -0.002
-MAX_TP_diff = 0.002
+MIN_TP_DIFF = -0.002
+MAX_TP_DIFF = 0.002
 ## Percent change colormap
 MIN_TP_PERCENT_CHANGE = -30
 MAX_TP_PERCENT_CHANGE = 30
-
-#--  Titles
-TITLE_TP_EPA = "TP_EPA"
-TITLE_TP_Mark = "TP_Mark"
-TITLE_TP_diff = "TP_diff"
-TITLE_TP_percent_change = "TP_Change"
-#--  UNITS
-UNITS_TP_EPA = "(mg/L)"
-UNITS_TP_Mark = "(mg/L)"
-UNITS_TP_diff = "(mg/L)"
-UNITS_TP_percent_change = "(%)"
+##-- Titles
+TITLE_TP_COMPARE = "TP_COMPARE"
+TITLE_TP_DIFF = "TP_diff"
+TITLE_TP_PERCENT_CHANGE = "TP_Change"
+##-- UNITS
+UNITS_TP_COMPARE = "(mg/L)"
+UNITS_TP_DIFF = "(mg/L)"
+UNITS_TP_PERCENT_CHANGE = "(%)"
 
 
 #----- Do not modify ------------
+
+##--- Error checking
+
+## Plot types: Pick at least 1
+if (do_3Dplot+do_2Dslice+do_2Dtransect) < 1:
+    sys.exit("Pick at least 1 plot in setparams.py.\n")
+
+
+## Name of run
+# Choose a string - no spaces! - to identify the run.  
+# It will be added to the name of the images.
+def set_RUN_NAME(): 
+    return RUN_NAME
+
 ## number of mi_ files
 ## todo:  set according to what is in the directory
 def set_NUM_MI_FILES():
@@ -62,6 +93,15 @@ def set_NUM_MI_FILES():
 # If not, it will do every single timestep of every file
 def set_do_first_in_file():
     return do_first_in_file
+
+# Are you comparing two runs? 
+def set_do_compare():
+    return do_compare
+
+# Is the comparison dataset from Mark
+# If so, TP will be defined in terms of NDZP
+def set_do_MDR():
+    return do_MDR
 
 ## Set which plot types using flags (0 or 1, do them or not)
 def set_do3Dplot():
@@ -82,17 +122,17 @@ def set_TO_Y():
     return TO_Y
 
 ## set min/max for colormap
-## for both TP_EPA and TP_Mark
+## for both TP_EPA and TP_COMPARE
 ### TP colormap
 def set_MIN_TP():
     return MIN_TP 
 def set_MAX_TP():
     return MAX_TP 
 ### TP difference colormap
-def set_MIN_TP_diff():
-    return MIN_TP_diff
-def set_MAX_TP_diff():
-    return MAX_TP_diff
+def set_MIN_TP_DIFF():
+    return MIN_TP_DIFF
+def set_MAX_TP_DIFF():
+    return MAX_TP_DIFF
 ### TP percent change colormap
 def set_MIN_TP_PERCENT_CHANGE():
     return MIN_TP_PERCENT_CHANGE
@@ -103,21 +143,21 @@ def set_MAX_TP_PERCENT_CHANGE():
 ### TP
 def set_TITLE_TP_EPA():
     return TITLE_TP_EPA 
-def set_TITLE_TP_Mark():
-    return TITLE_TP_Mark
-def set_TITLE_TP_diff():
-    return TITLE_TP_diff
-def set_TITLE_TP_percent_change():
-    return TITLE_TP_percent_change
+def set_TITLE_TP_COMPARE():
+    return TITLE_TP_COMPARE
+def set_TITLE_TP_DIFF():
+    return TITLE_TP_DIFF
+def set_TITLE_TP_PERCENT_CHANGE():
+    return TITLE_TP_PERCENT_CHANGE
 
 ## set titles
 ### TP
 def set_UNITS_TP_EPA():
     return UNITS_TP_EPA
-def set_UNITS_TP_Mark():
-    return UNITS_TP_Mark
-def set_UNITS_TP_diff():
-    return UNITS_TP_diff
-def set_UNITS_TP_percent_change():
-    return UNITS_TP_percent_change
+def set_UNITS_TP_COMPARE():
+    return UNITS_TP_COMPARE
+def set_UNITS_TP_DIFF():
+    return UNITS_TP_DIFF
+def set_UNITS_TP_PERCENT_CHANGE():
+    return UNITS_TP_PERCENT_CHANGE
 
