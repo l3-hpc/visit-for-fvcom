69624bd1dab71203fb8e6396ae82f68346a57ba6
Lisa Lowe <lllowe@ncsu.edu>
2022-04-29 09:38:02 -0400

    added test statements

--- test_params.py
+++ test_params.py
@@ -6,7 +6,7 @@
 import sys 
 
 #To calculate normal vector for defining transect
-import numpy as np
+#import numpy as np
 
 #This is user defined setpaths.py in the current working directory
 import setpaths
@@ -21,12 +21,13 @@ base_EPA_database = setpaths.set_EPA_path()
 
 # If you will compare 2 data sets
 do_compare = setparams.set_do_compare()
-if(do_compare):
-    base_COMPARE_database = setpaths.set_COMPARE_path()
-    base_conn_string = setpaths.set_conn_string()
-    #Will you compare against Mark's data, with NDZP?
-    do_MDR = setparams.set_do_MDR()
 
+#Will you compare against Mark's data, with NDZP?
+base_COMPARE_database = setpaths.set_COMPARE_path()
+base_conn_string = setpaths.set_conn_string()
+
+#for testing, always to that
+do_MDR = setparams.set_do_MDR()
 
 #Calls setpaths.py to define where the images are located
 IMGS_DIR = setpaths.set_image_path()
@@ -34,6 +35,10 @@ IMGS_DIR = setpaths.set_image_path()
 #How many mi files are available?
 #Change this to look at the path and calculate
 NUM_MI_FILES = setparams.set_NUM_MI_FILES()
+
+#For interactive...not used in plot_any
+MI_ID_INIT = setparams.set_MI_ID_INIT()
+
 
 # Just plot the first timestep of every mi file?
 # If not, it will do every single timestep of every file
@@ -43,7 +48,6 @@ do_first_in_file = setparams.set_do_first_in_file()
 do_3Dplot = setparams.set_do3Dplot()
 do_2Dslice = setparams.set_do2Dslice()
 do_2Dtransect = setparams.set_do2Dtransect()
-
 
 #Add mesh?
 add_mesh = True
@@ -57,30 +61,67 @@ TITLE_TP_EPA = setparams.set_TITLE_TP_EPA()
 #UNITS
 UNITS_TP_EPA = setparams.set_UNITS_TP_EPA()
 
-if(do_compare):
-    MIN_TP_DIFF = setparams.set_MIN_TP_DIFF()
-    MAX_TP_DIFF = setparams.set_MAX_TP_DIFF()
-    #Percent change
-    MIN_TP_PERCENT_CHANGE = setparams.set_MIN_TP_PERCENT_CHANGE()
-    MAX_TP_PERCENT_CHANGE = setparams.set_MAX_TP_PERCENT_CHANGE()
-    #Titles
-    TITLE_TP_COMPARE = setparams.set_TITLE_TP_COMPARE()
-    TITLE_TP_DIFF = setparams.set_TITLE_TP_DIFF()
-    TITLE_TP_PERCENT_CHANGE = setparams.set_TITLE_TP_PERCENT_CHANGE()
-    #UNITS
-    UNITS_TP_COMPARE = setparams.set_UNITS_TP_COMPARE()
-    UNITS_TP_DIFF = setparams.set_UNITS_TP_DIFF()
-    UNITS_TP_PERCENT_CHANGE = setparams.set_UNITS_TP_PERCENT_CHANGE()
+# testing, always do...if(do_compare):
+MIN_TP_DIFF = setparams.set_MIN_TP_DIFF()
+MAX_TP_DIFF = setparams.set_MAX_TP_DIFF()
+#Percent change
+MIN_TP_PERCENT_CHANGE = setparams.set_MIN_TP_PERCENT_CHANGE()
+MAX_TP_PERCENT_CHANGE = setparams.set_MAX_TP_PERCENT_CHANGE()
+#Titles
+TITLE_TP_COMPARE = setparams.set_TITLE_TP_COMPARE()
+TITLE_TP_DIFF = setparams.set_TITLE_TP_DIFF()
+TITLE_TP_PERCENT_CHANGE = setparams.set_TITLE_TP_PERCENT_CHANGE()
+#UNITS
+UNITS_TP_COMPARE = setparams.set_UNITS_TP_COMPARE()
+UNITS_TP_DIFF = setparams.set_UNITS_TP_DIFF()
+UNITS_TP_PERCENT_CHANGE = setparams.set_UNITS_TP_PERCENT_CHANGE()
 
 
 #For transects
-if (do_2Dtransect):
-    FROM_X = setparams.set_FROM_X() 
-    FROM_Y = setparams.set_FROM_Y()
-    TO_X = setparams.set_TO_X()
-    TO_Y = setparams.set_TO_Y()
+# testing, always do...if (do_2Dtransect):
+FROM_X = setparams.set_FROM_X() 
+FROM_Y = setparams.set_FROM_Y()
+TO_X = setparams.set_TO_X()
+TO_Y = setparams.set_TO_Y()
 
-print("do_compare")
-print(do_compare)
-print(type(do_compare))
-
+#print(setparams.test_param("",))
+print(setparams.test_param("RUN_NAME",RUN_NAME ))
+print(setparams.test_param("NUM_MI_FILES",NUM_MI_FILES))
+print(setparams.test_param("MI_ID_INIT",MI_ID_INIT))
+print(setparams.test_param("do_first_in_file",do_first_in_file))
+print(setparams.test_param("do_compare",do_compare))
+print(setparams.test_param("do_MDR",do_MDR))
+print(setparams.test_param("do_3Dplot",do_3Dplot))
+print(setparams.test_param("do_2Dslice",do_2Dslice))
+print(setparams.test_param("do_2Dtransect",do_2Dtransect))
+print(setparams.test_param("FROM_X",FROM_X))
+print(setparams.test_param("FROM_Y",FROM_Y))
+print(setparams.test_param("TO_X",TO_X))
+print(setparams.test_param("TO_Y",TO_Y))
+print(setparams.test_param("MIN_TP",MIN_TP))
+print(setparams.test_param("MAX_TP",MAX_TP))
+print(setparams.test_param("TITLE_TP_EPA",TITLE_TP_EPA))
+print(setparams.test_param("UNITS_TP_EPA",UNITS_TP_EPA))
+print(setparams.test_param("MIN_TP_DIFF",MIN_TP_DIFF))
+print(setparams.test_param("MAX_TP_DIFF",MAX_TP_DIFF))
+print(setparams.test_param("TITLE_TP_COMPARE",TITLE_TP_COMPARE))
+print(setparams.test_param("TITLE_TP_DIFF",TITLE_TP_DIFF))
+print(setparams.test_param("TITLE_TP_PERCENT_CHANGE",TITLE_TP_PERCENT_CHANGE))
+print(setparams.test_param("UNITS_TP_COMPARE",UNITS_TP_COMPARE))
+print(setparams.test_param("UNITS_TP_DIFF",UNITS_TP_DIFF))
+print(setparams.test_param("UNITS_TP_PERCENT_CHANGE",UNITS_TP_PERCENT_CHANGE))
+print(setparams.test_param("base_COMPARE_database",base_COMPARE_database))
+print(setparams.test_param("base_conn_string",base_conn_string))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
+#print(setparams.test_param("",))
